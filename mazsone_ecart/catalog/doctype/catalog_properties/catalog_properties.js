1a29067d30d2be6de7fb17a9c4e3bf791371dfa5
SUNDAR <sundar@codexsun.com>
2025-08-12 16:04:03 +0530

    #16 Catalog Product Spec

---
+++ mazsone_ecart/catalog/doctype/catalog_properties/catalog_properties.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aaran Software and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Catalog Properties", {
+// 	refresh(frm) {
+
+// 	},
+// });
