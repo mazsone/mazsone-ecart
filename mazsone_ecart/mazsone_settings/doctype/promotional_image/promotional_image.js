52213260f4f2166ad62d9bc8f664d1b1bd990bab
SUNDAR <sundar@codexsun.com>
2025-08-13 15:21:23 +0530

    #26 Catalog Details updated

---
+++ mazsone_ecart/mazsone_settings/doctype/promotional_image/promotional_image.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aaran Software and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Promotional Image", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ mazsone_ecart/mazsone_settings/doctype/slider_3/slider_3.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Aaran Software and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Slider 3", {
+// 	refresh(frm) {
+
+// 	},
+// });
