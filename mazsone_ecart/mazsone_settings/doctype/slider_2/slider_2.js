8b7f1de2c5d9ffaa9c84eeb07a0cd2e3e0d9b0d1
SUNDAR <sundar@codexsun.com>
2025-08-15 00:18:38 +0530

    #38 reworked on catalog

--- mazsone_ecart/mazsone_settings/doctype/slider_2/slider_2.js
+++ mazsone_ecart/mazsone_settings/doctype/slider_2/slider_2.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2025, Aaran Software and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Slider 2", {
-// 	refresh(frm) {
+frappe.ui.form.on("Slider 2", {
+	refresh(frm) {
 
-// 	},
-// });
+		        // List of image fieldnames to apply styling to
+        const imageFields = ['background_preview', 'product_preview']; // Add more as needed
+
+        imageFields.forEach(function(fieldname) {
+            const imageField = frm.fields_dict[fieldname];
+            if (imageField && imageField.$wrapper) {
+                imageField.$wrapper.find('img').css({
+                    'width': '250px',
+                    'height': '150px',
+                    'object-fit': 'contain' // or 'cover' depending on your use-case
+                });
+            }
+        });
+
+	},
+});
